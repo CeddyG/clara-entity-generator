0686a67695fb3acaa48ff23175bbfd80d3e23177
CeddyG <ceddyg@free.fr>
2019-03-08 11:44:45 +0100

    -Fix navbar generator

--- src/Generator/NavbarGenerator.php
+++ src/Generator/NavbarGenerator.php
@@ -2,8 +2,6 @@
 
 namespace CeddyG\ClaraEntityGenerator\Generator;
 
-use Config;
-
 class NavbarGenerator extends BaseGenerator
 {
     /*
@@ -27,19 +25,26 @@ class NavbarGenerator extends BaseGenerator
      */
     public function generate($sFolder = '')
     {
-        $aConfig            = config('clara.navbar', []);
+        $sFile = base_path().'/config/clara.navbar.php';
+        
+        $aConfig = file_exists($sFile) ? include $sFile : [];
         $aConfig[$sFolder]  = ucwords(str_replace('-', ' ', $sFolder));
         
-        Config::set('clara.navbar', $aConfig);
-        
-        $aNewConfig = [];
-        foreach ($aConfig as $sKey => $sValue)
-        {
-            $aNewConfig[] = "    '".$sKey."' => '".$sValue."'";
-        }
+        $sConfig = var_export($aConfig, true);
+        $sConfig = str_replace('  array (', '[', $sConfig);
+        $sConfig = str_replace('array (', '[', $sConfig);
+        $sConfig = str_replace('  [', '[', $sConfig);
+        $sConfig = str_replace(')', ']', $sConfig);
+        $sConfig = preg_replace('/[0-9] => \'/i', '\'', $sConfig);
+        $sConfig = preg_replace('/[0-9] => \n/i', '', $sConfig);
+        $sConfig = substr($sConfig, 1);
+        $sConfig = substr($sConfig, 0, -1);
+        $sConfig = str_replace("  \n", '', $sConfig);
+        $sConfig = str_replace('  ', '    ', $sConfig);
+        $sConfig = '    '.trim($sConfig);
         
         self::createFile('clara.navbar.php', [
-            'Config' => implode(",\n", $aNewConfig)
+            'Config' => $sConfig
         ]);
     }
 }
